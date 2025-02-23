a8e73537061ebfb4d8c03bda82fd1ff01e315f0b
Phe <phoomphat091@gmail.com>
2025-02-23 13:24:18 +0700

    Update Character Invisible.lua

--- Universe/Character Invisible.lua
+++ Universe/Character Invisible.lua
@@ -1,3 +1,9 @@
+local Workspace = game:GetService("Workspace")
+local Players = game:GetService("Players")
+local Plr = Players.LocalPlayer
+local invis_on = false
+local Clipon = false
+
 local function getKeyCode(keyName)
     return Enum.KeyCode[keyName]
 end
@@ -5,17 +11,17 @@ end
 _G.ScriptName = "Character Invisible"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/PHOOMPHAT-Git/Lua-Zone/refs/heads/main/dwcqweyhfbwregbfyiokgfnbac.lua'))()
 
-local invis_on = false
 function onKeyPress(inputObject, chat)
     if chat then return end
-    if inputObject.KeyCode == getKeyCode(_G.open_and_close) then
+    
+    if inputObject.KeyCode == getKeyCode(_G.Invisible) then
         invis_on = not invis_on
         if invis_on then
-            local savedpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+            local savedpos = Plr.Character.HumanoidRootPart.CFrame
             wait()
-            game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-25.95, 84, 3537.55))
+            Plr.Character:MoveTo(Vector3.new(-25.95, 84, 3537.55))
             wait(.15)
-            local Seat = Instance.new('Seat', game.Workspace)
+            local Seat = Instance.new('Seat', Workspace)
             Seat.Anchored = false
             Seat.CanCollide = false
             Seat.Name = 'invischair'
@@ -23,23 +29,66 @@ function onKeyPress(inputObject, chat)
             Seat.Position = Vector3.new(-25.95, 84, 3537.55)
             local Weld = Instance.new("Weld", Seat)
             Weld.Part0 = Seat
-            Weld.Part1 = game.Players.LocalPlayer.Character:FindFirstChild("Torso") or game.Players.LocalPlayer.Character.UpperTorso
+            Weld.Part1 = Plr.Character:FindFirstChild("Torso") or Plr.Character.UpperTorso
             wait()
             Seat.CFrame = savedpos
             game.StarterGui:SetCore("SendNotification", {
-                Title = "Invis On";
+                Title = "On : Invisible";
                 Duration = 1;
                 Text = "By Phoomphat";
             })
         else
-            workspace:FindFirstChild('invischair'):Remove()
+            Workspace:FindFirstChild('invischair'):Remove()
             game.StarterGui:SetCore("SendNotification", {
-                Title = "Invis Off";
+                Title = "Off : Invisible";
                 Duration = 1;
                 Text = "By Phoomphat";
             })
         end
     end
+
+    if inputObject.KeyCode == getKeyCode(_G.Noclip) then
+        if not Clipon then
+            Clipon = true
+            game.StarterGui:SetCore("SendNotification", {
+                Title = "On : Noclip";
+                Duration = 1;
+                Text = "By Phoomphat";
+            })
+            Stepped = game:GetService("RunService").Stepped:Connect(function()
+                if Clipon then
+                    for a, b in pairs(Workspace:GetChildren()) do
+                        if b.Name == Plr.Name then
+                            for i, v in pairs(Workspace[Plr.Name]:GetChildren()) do
+                                if v:IsA("BasePart") then
+                                    v.CanCollide = false
+                                end
+                            end
+                        end
+                    end
+                end
+            end)
+        else
+            Clipon = false
+            game.StarterGui:SetCore("SendNotification", {
+                Title = "Off : Noclip";
+                Duration = 1;
+                Text = "By Phoomphat";
+            })
+            if Stepped then
+                for _, b in pairs(Workspace:GetChildren()) do
+                    if b.Name == Plr.Name then
+                        for _, v in pairs(Workspace[Plr.Name]:GetChildren()) do
+                            if v:IsA("BasePart") and v.Name == "HumanoidRootPart" then
+                                v.CanCollide = true
+                            end
+                        end
+                    end
+                end
+                Stepped:Disconnect()
+            end
+        end
+    end
 end
 
-game:GetService("UserInputService").InputBegan:connect(onKeyPress)
+game:GetService("UserInputService").InputBegan:Connect(onKeyPress)
